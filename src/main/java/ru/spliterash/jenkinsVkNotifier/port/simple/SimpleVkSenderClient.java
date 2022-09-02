739954c4c14d143d99e857119ed691bf8f1d73f0
Spliterash <me@spliterash.ru>
2022-09-03 01:29:12 +0300

    Fix freezing

--- src/main/java/ru/spliterash/jenkinsVkNotifier/port/simple/SimpleVkSenderClient.java
+++ src/main/java/ru/spliterash/jenkinsVkNotifier/port/simple/SimpleVkSenderClient.java
@@ -1,11 +1,14 @@
 package ru.spliterash.jenkinsVkNotifier.port.simple;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.ConnectionConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -16,17 +19,26 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+@Log4j
 @RequiredArgsConstructor
 public class SimpleVkSenderClient implements VkSender {
-    private static final Random RANDOM = new Random();
-    private static final CloseableHttpClient client = HttpClients.custom()
-            .disableCookieManagement()
-            .build();
-
+    private static final int TIMEOUT = 1000 * 10;
     private final String token;
 
+
+    private CloseableHttpClient createClient() {
+        return HttpClients.custom()
+                .disableCookieManagement()
+                .setDefaultConnectionConfig(ConnectionConfig.DEFAULT)
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectionRequestTimeout(TIMEOUT)
+                        .setConnectTimeout(TIMEOUT)
+                        .setSocketTimeout(TIMEOUT)
+                        .build())
+                .build();
+    }
 
     @Override
     public void sendMessage(String peerId, String txt) {
@@ -36,11 +48,11 @@ public class SimpleVkSenderClient implements VkSender {
 
                 new BasicNameValuePair("peer_id", peerId),
                 new BasicNameValuePair("message", txt),
-                new BasicNameValuePair("random_id", String.valueOf(RANDOM.nextLong()))
+                new BasicNameValuePair("random_id", String.valueOf(ThreadLocalRandom.current().nextLong()))
         );
 
 
-        try {
+        try (CloseableHttpClient client = createClient()) {
             HttpPost post = new HttpPost("https://api.vk.com/method/messages.send");
             post.setEntity(new UrlEncodedFormEntity(data, StandardCharsets.UTF_8));
 
